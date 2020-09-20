239e02578f1b3f5ab7aa4ad4296d9e75d3db68b5
Amilcar-Steban <71573575+Amilcar-Steban@users.noreply.github.com>
2020-09-20 03:26:44 -0500

    tercer commit

--- src/model/Tintegradora.java
+++ src/model/Tintegradora.java
@@ -1,11 +1,18 @@
 package model;
 
-import java.util.Scanner;
 public class Tintegradora {
-    private static int ROUGH_CONSTRUCTION = 1300000;
-    private static int FINAL_CONSTRUCTION = 2600000;
-    private static int PAINTING = 980000;
+    private static final int ROUGH_CONSTRUCTION = 1300000;
+    private static final int FINAL_CONSTRUCTION = 2600000;
+    private static final int PAINTING = 980000;
     
+    /**
+     * method that calculate the total of each material
+     * <b> Pre: </b> hCenter and Quantity must have a value and must be initialized<br>
+     * <b> Pos: </b> returns a value total  <br>
+     * @param hCenter != null
+     * @param Quantity != null
+     * @return ValorHC return a value total of price od material and quantity in a array
+     */
     public static double[] TotalHC(double hCenter[],int Quantity[]){
     double[] ValorHC = new double[Quantity.length];
         for (int i = 0; i <ValorHC.length; i++) {
@@ -13,6 +20,14 @@ public class Tintegradora {
         }
     return ValorHC;
     }
+    /**
+     * method that calculate the total of each material
+     * <b> Pre: </b> ValorFB and Quantity must have a value and must be initialized<br>
+     * <b> Pos: </b> returns a value total  <br>
+     * @param fBarrio
+     * @param Quantity
+     * @return ValorFB return a value total of price od material and quantity in a array
+     */
     public static double[] TotalFB(double fBarrio[],int Quantity[]){
     double[] ValorFB = new double[Quantity.length];
         for (int i = 0; i <ValorFB.length; i++) {
@@ -20,6 +35,14 @@ public class Tintegradora {
         }
     return ValorFB;
     }
+    /**
+     * method that calculate the total of each material
+     * <b> Pre: </b> ValorFC and Quantity must have a value and must be initialized<br>
+     * <b> Pos: </b> returns a value total  <br>
+     * @param fCentro
+     * @param Quantity
+     * @return ValorFC return a value total of price od material and quantity in a array
+     */
     public static double[] TotalFC(double fCentro[],int Quantity[]){
     double[] ValorFC = new double[Quantity.length];
         for (int i = 0; i <ValorFC.length; i++) {
@@ -27,6 +50,15 @@ public class Tintegradora {
         }
     return ValorFC;
     }
+    /**
+     * method that calculate the sum of alls the hardware store
+     * <b> Pre: </b> THC, TFB, TFC, aux and total must have a value and must be initialized<br>
+     * <b> Pos: </b> returns a value total of the work <br>
+     * @param THC != null
+     * @param TFB != null
+     * @param TFC != null
+     * @return a value total of the w
+     */
     public static double ValorTotal(double[] THC, double[] TFB, double[] TFC){
         double total = 0;
         double aux = 0;
@@ -36,6 +68,14 @@ public class Tintegradora {
         }
     return aux;
     }
+    /**
+     * method that sum the value total od hardware store with the addresses
+     * <b> Pre: </b> direction, total and vtotal must have a value and must be initialized<br>
+     * <b> Pos: </b> returns a value total of the work plus the address <br>
+     * @param direction != null
+     * @param total != null
+     * @return vtotal return a total of work
+     */
     public static double DirectionU(int direction, double total){
         double vtotal = 0;
         if (direction == 1){
@@ -70,10 +110,27 @@ public class Tintegradora {
                 }
     return vtotal;
     }
+    /**
+     * method that calculate the sum of constant, addresses and the hardware store
+     * <b> Pre: </b> valorFinal and vtotal must be initialized<br>
+     * <b> Pos: </b> returns a value total of the work with constant and address <br>
+     * @param vtotal != null
+     * @return valorFinal value total
+     */
     public static double ValorFinal(double vtotal){
         double valorFinal = vtotal + ROUGH_CONSTRUCTION + FINAL_CONSTRUCTION + PAINTING;
         return valorFinal;
     }
+    /**
+     * method that calculate the best price of each material in each hardware store
+     * <b> Pre: </b> hCenter, fBarrio, fCentro, Arreglo and Mvalor must be initialized<br>
+     * <b> Pos: </b> return the best value of each material  <br>
+     * @param hCenter != null
+     * @param fBarrio != null
+     * @param fCentro != null
+     * @param Arreglo != null
+     * @return Mvalor return the best price of a material specific
+     */
     public static double[] Mejorprecio(double hCenter[], double fBarrio[], double fCentro[], String Arreglo[]){
         double[] Mvalor = new double[hCenter.length];
         for (int i = 0; i < hCenter.length; i++) {
@@ -89,6 +146,16 @@ public class Tintegradora {
         }
     return Mvalor;
     }
+    /**
+     * method that calculate the best hardware store for each material 
+     * <b> Pre: </b> hCenter, fBarrio, fCentro, Arreglo and lugar must be initialized<br>
+     * <b> Pos: </b> return the best hardware for each material  <br>
+     * @param hCenter != null
+     * @param fBarrio != null
+     * @param fCentro != null
+     * @param Arreglo != null
+     * @return lugar return a string of words 
+     */
     public static String[] MejorLugar(double hCenter[], double fBarrio[], double fCentro[], String Arreglo[]){
         String[] lugar = new String[Arreglo.length];
         for (int i = 0; i < hCenter.length; i++) {
@@ -104,6 +171,13 @@ public class Tintegradora {
         }
     return lugar;
     }
+    /**
+     * method that calculate the value total of the best prices
+     * <b> Pre: </b>  Mvalor, totalMPrecios and aux must be initialized<br>
+     * <b> Pos: </b> return the total of best prices in a double  <br>
+     * @param Mvalor != null and >0
+     * @return totalMPrecios return a value total in a double
+     */
     public static double totalMPrecios(double Mvalor[]){
         double totalMPrecios = 0;
         double aux = 0;
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -5,38 +5,57 @@ public class Main {
     Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int Cantidad = Main.Pedircantidad();
-        String[] arreglo1 = Main.listmaterials(Cantidad);
+        int QuantityM = Main.Pedircantidad();
+        String[] arreglo1 = Main.listmaterials(QuantityM);
         int[] utilidadEntra = Main.utilidad(arreglo1);        
-        double hCenter[] = Main.PricesHomeCenter(arreglo1, Cantidad);
-        double fBarrio[] = Main.PricesFNeighborhood(arreglo1, Cantidad);
-        double fCentro[] = Main.PricesFCenter(arreglo1, Cantidad);
+        double hCenter[] = Main.PricesHomeCenter(arreglo1, QuantityM);
+        double fBarrio[] = Main.PricesFNeighborhood(arreglo1, QuantityM);
+        double fCentro[] = Main.PricesFCenter(arreglo1, QuantityM);
         int Quantity[] = Main.quantityMaterials(arreglo1);
         double[] THC = Tintegradora.TotalHC(hCenter, Quantity);
         double[] TFB = Tintegradora.TotalFB(fBarrio, Quantity);
         double[] TFC = Tintegradora.TotalFC(fCentro, Quantity);
         double total = Tintegradora.ValorTotal(THC, TFB, TFC); //valor suma 3 tiendas
-        int direction = Main.pedirdireccion();
-        double vtotal = Tintegradora.DirectionU(direction, total);
+        int address = Main.pedirdireccion();
+        double vtotal = Tintegradora.DirectionU(address, total);
         double valorFinal = Tintegradora.ValorFinal(vtotal);
+        
         Main.mostrartotal(valorFinal);
+        
         double[] Mvalor = Tintegradora.Mejorprecio(hCenter, fBarrio, fCentro, arreglo1);
         String[] lugar = Tintegradora.MejorLugar(hCenter, fBarrio, fCentro, arreglo1);
+        
         Main.mostarMprecios(Mvalor, arreglo1, lugar);
+        
         double totalMPrecios = Tintegradora.totalMPrecios(Mvalor);
-        double vtotalMP = Tintegradora.DirectionU(direction, totalMPrecios);
+        double vtotalMP = Tintegradora.DirectionU(address, totalMPrecios);
+        
         Main.MostrarMPrecios(vtotalMP);   
         Main.MListObraNegra(utilidadEntra, arreglo1);
         Main.MListObraBlanca(arreglo1, utilidadEntra);
         Main.MListPintura(arreglo1, utilidadEntra);
     }
+    /**
+     * method to enter a quantity of materials
+     * <b> pre: </b> 
+     * <b> pos: </b> returns the quantity of materials <br> 
+     * @return Quantity>0
+     * @param: Quantity store quantity of materials. Quantity>0
+     * */
     public static int Pedircantidad(){
         Scanner sc = new Scanner(System.in);
         System.out.println("---------------------------------");
         System.out.print("Dijite la cantidad de materiles: ");
-        int Cantidad = sc.nextInt();
-    return Cantidad;
+        int Quantity = sc.nextInt();
+    return Quantity;
     }
+    /**
+     * method to say the names of each material
+     * <b> Pre: </b> num must have a value and must be initialized<br>
+     * <b> Pos: </b> returns a list of names <br>
+     * @param num is a call to Quantity. num>0 and !=null
+     * @return return a list of materials
+     */
     public static String[] listmaterials(int num){
         Scanner sc = new Scanner(System.in);
         String[] list = new String[num];
@@ -48,6 +67,13 @@ public class Main {
         }
     return list;
     }
+    /**
+     * method to select the usefulness of materials
+     * <b> Pre: </b> arreglo1 must be initialized  and must have a value<br>
+     * <b> Pos: </b> returns an option as a number using a menu <br>
+     * @param arreglo1 is the list of materials and !=null
+     * @return Outilidad return a value. Outilidad>0, !=null and <3
+     */
     public static int[] utilidad(String arreglo1[]){
         Scanner sc = new Scanner(System.in);            
         int[] Outilidad = new int[arreglo1.length];
@@ -62,7 +88,14 @@ public class Main {
         }
     return Outilidad;
     }
-    
+    /**
+     * method where it receives values ​​for each material
+     * <b> Pre: </b> Arreglo, ferreteriaCentro and num must be initialized and must have a value<br>
+     * <b> Pos: </b> return a list of values for each material <br>
+     * @param Arreglo material's list. != null
+     * @param num>0 and != null
+     * @return ferreteriaCentro the list of values
+     */
     public static double[] PricesFCenter(String Arreglo[], int num){
         Scanner sc = new Scanner(System.in);
         double[] ferreteriaCentro = new double[Arreglo.length];
@@ -74,6 +107,14 @@ public class Main {
         }
         return ferreteriaCentro;
     }
+    /**
+     * method where it receives values ​​for each material
+     * <b> Pre: </b> Arreglo, ferreteriaBarrio and num must be initialized and must have a value<br>
+     * <b> Pos: </b> return a list of values for each material <br>
+     * @param Arreglo material's list. !=null
+     * @param num>0 and != null
+     * @return ferreteriaBarrio the list of values
+     */
     public static double[] PricesFNeighborhood(String Arreglo[], int num){
         Scanner sc = new Scanner(System.in);
         double[] ferreteriaBarrio = new double[num];
@@ -85,6 +126,15 @@ public class Main {
         }
         return ferreteriaBarrio;
     }
+    /**
+     * method where it receives values ​​for each material
+     * <b> Pre: </b> Arreglo, homeCenter and num must be initialized and must have a value <br>
+     * <b> Pos: </b> return a list of values for each material <br>
+     * @param Arreglo material's list. !=nul
+     * @param num>0 and != null     
+     * @return homeCenter the list of values
+     */
+    
     public static double[] PricesHomeCenter( String Arreglo[], int num){
         Scanner sc = new Scanner(System.in);
         double[] homeCenter = new double[num];
@@ -96,6 +146,13 @@ public class Main {
         }
     return homeCenter;
     }
+    /**
+     * method where the quantity of each material is said
+     * <b> Pre: </b> Arreglo and Quantity1 must be initialized and must have a value<br>
+     * <b> Pos: </b> return a list of values for each material <br>
+     * @param Arreglo material's list. !=null
+     * @return homeCenter the list of values
+     */
     public static int[] quantityMaterials(String Arreglo[]){
         Scanner sc = new Scanner(System.in);
         int[] Quantity1 = new int[Arreglo.length];
@@ -107,6 +164,12 @@ public class Main {
         }
         return Quantity1;
     }
+    /**
+     * method to select the address
+     * <b> Pre: </b> address must be initialized  and must have a value<br>
+     * <b> Pos: </b> returns an option as a number using a menu <br>
+     * @return address return a value. address>0, !=null and <3
+     */
     public static int pedirdireccion(){
         Scanner sc = new Scanner(System.in);
         System.out.println("-----------------------");
@@ -114,13 +177,27 @@ public class Main {
         System.out.println("1.Norte");
         System.out.println("2.Centro");
         System.out.println("3.Sur");
-        int direction = sc.nextInt();
-    return direction;
+        int address = sc.nextInt();
+    return address;
     }
+    /**
+     * method to show total cost of the work
+     * <b> Pre: </b> valorFinal must be initialized and must have a value <br>
+     * <b> Pos: </b> shows the cost of the work <br>
+     * @param valorFinal != null
+     */
     public static void mostrartotal(double valorFinal){
         System.out.println("--------------");
         System.out.println("Valor Total: "+valorFinal);
     }
+    /**
+     * method to show the best prices depending on the hardware store
+     * <b> Pre: </b> Mvalor, arreglo1 and lugar must be initialized and must have a value<br>
+     * <b> Pos: </b> save the lowest prices of each material <br>
+     * @param Mvalor !=null
+     * @param arreglo1 !=null
+     * @param lugar  !=null
+     */
     public static void mostarMprecios(double Mvalor[], String arreglo1[], String lugar[]){
         System.out.print("-----------------");
         System.out.println("\nMEJORES PRECIOS");
@@ -128,33 +205,59 @@ public class Main {
             System.out.println("Mejor precio de "+arreglo1[i]+" en "+lugar[i]+": "+Mvalor[i]);
         }
     }
+    /**
+     * method for show the best prices
+     * <b> Pre: </b>  vtotalMP must be initialized and must have a value<br>
+     * <b> Pos: </b> shows the total of the best prices including the value per address <br>
+     * @param vtotalMP 
+     */
     public static void MostrarMPrecios(double vtotalMP){
             System.out.println("Total: "+ vtotalMP);
     }
-    
+    /**
+     * method showing materials classified by utility
+     * <b> Pre: </b>  utilidadEntra and arreglo1 must be initialized and  must have a value<br>
+     * <b> Pos: </b> displays materials in a list depending on their usefulness <br>
+     * @param utilidadEntra != null
+     * @param arreglo1 != null
+     */
     public static void MListObraNegra( int[] utilidadEntra, String[] arreglo1){
         System.out.println("----------------------------------");
         System.out.println("LISTA DE MATERIALES POR UTILIDAD");
         System.out.println("----------------------");
-        System.out.println("OBRA NEGRA :");
+        System.out.println("OBRA NEGRA:");
         for (int i = 0; i < arreglo1.length; i++) {
             if (utilidadEntra[i] == 1){
                 System.out.println("- "+arreglo1[i]);
             }
         }
     }
+    /**
+     * method showing materials classified by utility
+     * <b> Pre: </b>  utilidadEntra and arreglo1 must be initialized and  must have a value<br>
+     * <b> Pos: </b> displays materials in a list depending on their usefulness <br>
+     * @param arreglo1 != null
+     * @param utilidadEntra  != null
+     */
     public static void MListObraBlanca(String[] arreglo1, int[] utilidadEntra){
         System.out.println("----------------------");
-        System.out.println("OBRA BLANCA");
+        System.out.println("OBRA BLANCA:");
         for (int i = 0; i <arreglo1.length; i++) {
             if (utilidadEntra[i] == 2){
                 System.out.println("- "+arreglo1[i]);  
             }
         }
     }
+    /**
+     * method showing materials classified by utility
+     * <b> Pre: </b>  utilidadEntra and arreglo1 must be initialized and  must have a value<br>
+     * <b> Pos: </b> displays materials in a list depending on their usefulness <br>
+     * @param arreglo1 != null
+     * @param utilidadEntra != null
+     */
     public static void MListPintura(String[] arreglo1, int[] utilidadEntra){
         System.out.println("----------------------");
-        System.out.println("PINTURA");
+        System.out.println("PINTURA:");
         for (int i = 0; i <arreglo1.length; i++) {
             if (utilidadEntra[i] == 3){
                 System.out.println("- "+arreglo1[i]);  
